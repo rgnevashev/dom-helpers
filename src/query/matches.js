af9e8e986aa5b44effcd9943da1d7f85b079d80b
Roman Karmatsky <rgnevashev@gmail.com>
2017-08-29 11:28:39 +0300

    fix20

--- src/query/matches.js
+++ src/query/matches.js
@@ -1,28 +1,39 @@
-
 import canUseDOM from '../util/inDOM'
 import qsa from './querySelectorAll'
 
 let matches;
 if (canUseDOM) {
-  let body = document.body
-  let nativeMatch = body.matches
-                 || body.matchesSelector
-                 || body.webkitMatchesSelector
-                 || body.mozMatchesSelector
-                 || body.msMatchesSelector;
+  matches = function () {
 
-  matches = nativeMatch
-    ? (node, selector) => nativeMatch.call(node, selector)
-    : ie8MatchesSelector
+    function ie8MatchesSelector(node, selector) {
+      var matches = qsa(node.document || node.ownerDocument, selector)
+        , i = 0;
+
+      while (matches[i] && matches[i] !== node) i++;
+
+      return !!matches[i];
+    }
+
+    let fn = null;
+    return function (node, selector) {
+      if (fn === null) {
+        let body = document.body
+
+        if (body) {
+          let nativeMatch = body.matches
+                       || body.matchesSelector
+                       || body.webkitMatchesSelector
+                       || body.mozMatchesSelector
+                       || body.msMatchesSelector;
+
+          fn = nativeMatch
+          ? nativeMatch
+          : ie8MatchesSelector;
+        }
+      }
+      return fn.call(node, selector)
+    }
+  }();
 }
 
 export default matches
-
-function ie8MatchesSelector(node, selector) {
-  var matches = qsa(node.document || node.ownerDocument, selector)
-    , i = 0;
-
-  while (matches[i] && matches[i] !== node) i++;
-
-  return !!matches[i];
-}
